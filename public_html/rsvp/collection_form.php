db21046a165fe7a35714e8ff0a723c2cbc76fccd
Eric Turner <elturner@eecs.berkeley.edu>
2016-02-28 18:45:22 -0800

    styled the rsvp collection form.  should now be a working version.

--- public_html/rsvp/collection_form.php
+++ public_html/rsvp/collection_form.php
@@ -25,25 +25,53 @@ if (!empty($_POST)) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html class="no-js">
 
-<head>
-    <meta charset="utf-8">
-    <title>RSVP</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?= Configuration::$STATIC_PATH; ?>styles/normalize.css">
-    <style media="screen">
-        html, body {
-            font-family: -apple-system, avenir, 'avenir next', roboto, 'helvetica neue', helvetica, ubuntu, arial, sans-serif;
-        }
-    </style>
-</head>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Sam and Eric's Wedding</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
 
-<body>
-    	<header>
-        	<h1>RSVP</h1>
-			<h2>To Sam &amp; Eric's Wedding</h2>
-    	</header>
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<style>
+			body {
+				padding-top: 4em;
+				padding-bottom: 2.5em;
+			}
+		</style>
+		<link rel="stylesheet" href="/css/bootstrap-responsive.min.css">
+		<link rel="stylesheet" href="/css/main.css">
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic' rel='stylesheet' type='text/css'>
+
+	</head>
+	<body>
+		<center>
+		<div class="container">
+			
+			<!-- linkbar -->
+			<ul class="nav nav-pills pull-left">
+	 		  <li><a href="/index.html">Home</a></li>
+			  <li><a href="/venue.html">Venue</a></li>
+			  <li><a href="/accommodations.html">Accommodations</a></li>
+			  <li><a href="/registry.html">Registry</a></li>
+			  <li class="active">
+			  	<a href="/rsvp.html">RSVP</a>
+			  </li>
+		  	</ul>
+		  	<br />
+		  	<br />
+			
+			<div class="hero-unit">
+				<div class="row">
+
+					<!-- ************* -->
+
+					<h3>RSVP</h3>
+					<hr />
+					<br />
+
     <main>
 
 <?
@@ -60,8 +88,10 @@ if ($submitted && $success) {
             <label>Please list the people in your party:</label>
             <input type="text" name="people">
 			<br>
+			<br>
 	    <label>Total number of people in party:</label>
             <input type="text" name="partysize">
+			<br>
 			<br>
             <label>Can you make it?</label>
 			<select name="is_attending">
@@ -70,12 +100,14 @@ if ($submitted && $success) {
 				<option value="NO">Alas, no</option>
 			</select>
 			<br>
+			<br>
 	    <label>Email Address</label>
             <input type="email" name="contact_email">
 			<br>
-	    <label for="extra_info">Anything else we should know?</label>
 			<br>
-			<textarea name="extra_info" placeholder="..."></textarea>
+	    <label for="extra_info">Anything else we should know?</label>
+			<textarea name="extra_info" rows="5" placeholder="..."></textarea>
+			<br>
 			<br>
             <button type="submit" name="submit">Submit RSVP</button>
         </form>
@@ -83,8 +115,34 @@ if ($submitted && $success) {
 <? } // End show HTML form ?>
 
     </main>
-    <br>
-    <br>
-</body>
 
+
+					<!-- ************* -->
+				
+					<br />
+					<hr />
+					<a href="&#109;&#97;&#105;&#108;&#116;&#111;:&#105;&#110;&#102;&#111;&#64;&#115;&#97;&#109;&#97;&#110;&#100;&#101;&#114;&#105;&#99;&#46;&#111;&#114;&#103;">&#105;&#110;&#102;&#111;&#64;&#115;&#97;&#109;&#97;&#110;&#100;&#101;&#114;&#105;&#99;&#46;&#111;&#114;&#103;</a>
+
+					<!-- ************* -->
+				</div>
+			</div>
+
+			<hr>
+
+			<footer>
+				<font color="#FFFFFF">
+					<p>&copy; Eric Turner and Samantha Shropshire 2016</p>
+				</font>
+			</footer>
+
+		</div> <!-- /container -->
+		</center>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+		<script src="js/vendor/bootstrap.min.js"></script>
+
+		<script src="js/main.js"></script>
+	</body>
 </html>
